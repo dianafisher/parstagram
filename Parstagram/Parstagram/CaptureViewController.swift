0e3a77b06f30a87caeca22892c236ce3b2f49545
Diana Fisher <diana.fisher@gmail.com>
2018-12-31 20:46:40 -0800

    Post image to Parse server.

--- Parstagram/Parstagram/CaptureViewController.swift
+++ Parstagram/Parstagram/CaptureViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Parse
 
 class CaptureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var capturedImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,17 +20,112 @@ class CaptureViewController: UIViewController, UIImagePickerControllerDelegate, 
     }
     
     @IBAction func onTapped(_ sender: UITapGestureRecognizer) {
-        print("Tapped")
+        
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            vc.sourceType = UIImagePickerController.SourceType.camera
+        } else {
+            vc.sourceType = UIImagePickerController.SourceType.photoLibrary
+        }
+        
+        self.present(vc, animated: true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Get the image captured by the UIImagePickerController
+//        let originalImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        let editedImage = info[UIImagePickerController.InfoKey.editedImage] as! UIImage
+        
+        // Do something with images
+        capturedImageView.image = editedImage
+        
+        // Dismiss UIImagePickerController
+        dismiss(animated: true, completion: nil)
     }
-    */
+    
+    func resize(image: UIImage, newSize: CGSize) -> UIImage? {        
+        let resizeImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        resizeImageView.contentMode = UIView.ContentMode.scaleAspectFill
+        resizeImageView.image = image
+        
+        UIGraphicsBeginImageContext(resizeImageView.frame.size)
+        resizeImageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+    @IBAction func onSubmitPressed(_ sender: Any) {
+        let image = capturedImageView.image
+        let caption = "Placeholder"
+        Post.postUserImage(image: image, withCaption: caption) { (success, error) in
+            if (success) {
+                print("Success!!")
+            } else {
+                print("ERROR: \(String(describing: error?.localizedDescription))")
+            }
+        }
+    }
+}
 
+class Post: PFObject, PFSubclassing {
+    @NSManaged var media : PFFileObject
+    @NSManaged var author: PFUser
+    @NSManaged var caption: String
+    @NSManaged var likesCount: Int
+    @NSManaged var commentsCount: Int
+    
+    /* Needed to implement PFSubclassing interface */
+    class func parseClassName() -> String {
+        return "Post"
+    }
+    
+    /**
+     * Other methods
+     */
+    
+    /**
+     Method to add a user post to Parse (uploading image file)
+     
+     - parameter image: Image that the user wants upload to parse
+     - parameter caption: Caption text input by the user
+     - parameter completion: Block to be executed after save operation is complete
+     */
+    class func postUserImage(image: UIImage?, withCaption caption: String?, withCompletion completion: PFBooleanResultBlock?) {
+        // use subclass approach
+        let post = Post()
+        
+        guard let currentUser = PFUser.current() else { return }
+        guard let media = getPFFileFromImage(image: image) else { return }
+        // Add relevant fields to the object
+        post.media = media
+        post.author = currentUser
+        post.caption = caption ?? ""
+        post.likesCount = 0
+        post.commentsCount = 0
+        
+        // Save object (following function will save the object in Parse asynchronously)
+        post.saveInBackground(block: completion)
+    }
+    
+    /**
+     Method to convert UIImage to PFFile
+     
+     - parameter image: Image that the user wants to upload to parse
+     
+     - returns: PFFile for the the data in the image
+     */
+    class func getPFFileFromImage(image: UIImage?) -> PFFileObject? {
+        // check if image is not nil
+        if let image = image {
+            // get image data and check if that is not nil
+            if let imageData = image.pngData() {
+                return PFFileObject(name: "image.png", data: imageData)
+            }
+        }
+        return nil
+    }
 }
