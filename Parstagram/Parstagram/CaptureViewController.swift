a5652f6694e1ecd0df103aa219324cd1b04f6ec5
Diana Fisher <diana.fisher@gmail.com>
2019-02-10 10:02:21 -0800

    Present CaptureViewController modally.

--- Parstagram/Parstagram/CaptureViewController.swift
+++ Parstagram/Parstagram/CaptureViewController.swift
@@ -74,7 +74,7 @@ class CaptureViewController: UIViewController, UIImagePickerControllerDelegate, 
         post.saveInBackground { (success, error) in
             if (success) {
                 print("Successfully posted!")
-                self.navigationController?.popViewController(animated: true)
+                self.dismiss(animated: true, completion: nil)
             } else {
                 print("ERROR: \(String(describing: error?.localizedDescription))")
             }
